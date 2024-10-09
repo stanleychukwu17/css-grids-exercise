bfffca59a2d134ef89d31fcccbaa61d7e8c5dea7
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-10-09 20:07:41 +0100

    cleaned up

--- start/95-responsive-grid-using-grid-template-areas/main.js
+++ start/95-responsive-grid-using-grid-template-areas/main.js
@@ -1,7 +1,4 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -20,5 +17,3 @@ document.querySelector('#app').innerHTML = `
     </p>
   </div>
 `
-
-setupCounter(document.querySelector('#counter'))
